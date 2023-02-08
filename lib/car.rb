bdd279984b75e912d7dc93d979d9a0c8c861b50d
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-08 09:29:35 -0700

    Pass test 1

---
+++ lib/car.rb
@@ -0,0 +1,18 @@
+class Car
+
+  def initialize(make_model, monthly_payment, loan_length)
+    @make_model = make_model
+    @monthly_payment = monthly_payment
+    @loan_length = loan_length
+  end
+
+  
+
+
+
+
+
+
+
+  
+end
