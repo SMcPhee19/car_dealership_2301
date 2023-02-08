cf414a784423e970deec4bc8e199d0bc2c88f017
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-08 09:58:13 -0700

    Finish test 2

--- lib/car.rb
+++ lib/car.rb
@@ -1,12 +1,14 @@
 class Car
-
+  attr_reader :make_model
   def initialize(make_model, monthly_payment, loan_length)
     @make_model = make_model
     @monthly_payment = monthly_payment
     @loan_length = loan_length
   end
 
-  
+  def make
+    @make_model.split[0]
+  end
 
 
 
